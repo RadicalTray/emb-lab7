e79909f17f47868da83eef2982b0e435533ad76e
RadicalTray <hydrochloricmahn@gmail.com>
2024-10-03 17:29:24 +0700

    .

--- recv.py
+++ recv.py
@@ -3,18 +3,14 @@ import time
 import hashlib
 
 # N = 1_000_000
-N = 100_000
+N = 1_000
 
 h = hashlib.new('md5')
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 ser.flush()
 data = ser.read(1)
 starttime = time.time()
-while len(data) < N:
-    newdat = ser.read(1)
-    # newdat = ser.read(N - len(data))
-    data = data + newdat
-    print(f'{len(data)}:', newdat)
+data = data + ser.read(N - len(data))
 stoptime = time.time()
 h.update(data)
 print(f'Data length is {len(data)}\nMD5 is : {h.hexdigest()}')
@@ -24,4 +20,4 @@ hash_dump = open('recv.md5', 'w')
 print(h.hexdigest(), file=hash_dump)
 
 data_dump = open('recv_data', 'wb')
-print(data, file=data_dump)
+data_dump.write(data)
--- send.py
+++ send.py
@@ -4,7 +4,7 @@ from random import randbytes
 import time
 
 # N = 1_000_000
-N = 100_000
+N = 1_000
 
 datagen = randbytes(N)
 
@@ -14,17 +14,21 @@ print('MD5 is :', h.hexdigest())
 
 ser = serial.Serial('/dev/ttyUSB1', 9600)
 
-i = 0;
+batchSize = 1024
+i = 0
+
+batch = datagen[i : i + batchSize]
+ser.write(batch)
+i += len(batch)
+
 while i < N:
-    batchSize = 1024
+    time.sleep(0.1)
     batch = datagen[i : i + batchSize]
-    print(batch)
     ser.write(batch)
     i += len(batch)
-    time.sleep(0.1)
 
 hash_dump = open('send.md5', 'w')
 print(h.hexdigest(), file=hash_dump)
 
 data_dump = open('send_data', 'wb')
-print(datagen, file=data_dump)
+data_dump.write(datagen)
